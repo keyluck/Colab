b165b34df6f7ba7b05e76cd7259634d424a0f1c8
Nick Luckey <38598401+keyluck@users.noreply.github.com>
2020-07-13 13:00:24 -0700

    Deploy to Heroku

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport) {
         new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback'
+            callbackURL: 'http://co-lab-app.herokuapp.com/auth/google/callback'
             },
         async (accessToken, refreshToken, profile, done) => {
             const newUser = {
